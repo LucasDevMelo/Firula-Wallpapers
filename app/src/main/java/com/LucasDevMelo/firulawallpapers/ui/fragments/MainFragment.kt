a83698da6c0db766b077130c86ea3d52439be732
LucasDevMelo <lucaslopesdemelosilva@gmail.com>
2023-09-11 23:09:59 -0300

    Initial commit

---
+++ app/src/main/java/com/LucasDevMelo/firulawallpapers/ui/fragments/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.LucasDevMelo.firulawallpapers.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.LucasDevMelo.firulawallpapers.R
+import com.LucasDevMelo.firulawallpapers.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+}
