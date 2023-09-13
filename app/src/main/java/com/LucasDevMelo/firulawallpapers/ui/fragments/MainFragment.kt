cb9b0034782d3a0bc374c0cea3116e5c533b1c8c
LucasDevMelo <lucaslopesdemelosilva@gmail.com>
2023-09-12 23:19:05 -0300

    atualizacao 12/09

--- app/src/main/java/com/LucasDevMelo/firulawallpapers/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/LucasDevMelo/firulawallpapers/ui/fragments/MainFragment.kt
@@ -5,14 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.LucasDevMelo.firulawallpapers.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.LucasDevMelo.firulawallpapers.databinding.FragmentMainBinding
+import com.LucasDevMelo.firulawallpapers.ui.fragments.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
 
-
     private val tabTitles = listOf("Início", "Popular", "Aleatório", "Categorias")
-    private val  fragment = listOf()
+    private val fragments = listOf(HomeFragment(), PopularFragment(), RandomFragment(), CategoriesFragment())
 
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
@@ -20,6 +22,24 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(layoutInflater,container,false)
+        initViewPager()
+        initToolBar()
         return binding.root
+    }
+
+    private fun initViewPager() {
+        val pagerAdapter = ViewPagerAdapter(context as FragmentActivity, fragments)
+        binding.viewPager.adapter = pagerAdapter
+        binding.viewPager.isUserInputEnabled = false
+
+        // Agora, após definir o adaptador, você pode configurar o TabLayoutMediator.
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+
+    private fun initToolBar(){
+        binding.toolbar.title = "Wallpapers"
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
     }
 }
